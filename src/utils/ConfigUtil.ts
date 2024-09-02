89e3e722112caba6e015c3e7a2a7e81071a15972
Malnati <ricardomalnati@gmail.com>
2024-09-02 10:11:11 -0300

    Refactor ConfigUtil to remove 'types' component option

--- src/utils/ConfigUtil.ts
+++ src/utils/ConfigUtil.ts
@@ -22,7 +22,7 @@ export class ConfigUtil {
             .requiredOption('-d, --data <type>', 'Corpo da requisição (JSON)', '')
             .requiredOption('-a, --app <type>', 'Nome da aplicação')
             .requiredOption('-o, --outputDir <type>', 'Diretório de saída para os arquivos gerados', './build')
-            .requiredOption('-f, --components <type>', 'Especifique quais componentes gerar (component, styles, hooks, types, services, validation, context, readme)', 'component')
+            .requiredOption('-f, --components <type>', 'Especifique quais componentes gerar (component, styles, hooks, services, validation, context)', 'component')
             .option('--dependencies <items>', 'Dependências para instalar', (value) => value.split(','), [])
             .option('--devDependencies <items>', 'DevDependencies para instalar', (value) => value.split(','), [])
             .parse(process.argv);
@@ -36,9 +36,9 @@ export class ConfigUtil {
             body: options.data,
         };
 
-        const components: ['component' | 'styles' | 'hooks' | 'types' | 'services' | 'validation' | 'context' | 'readme'] = options.components
+        const components: ['component' | 'styles' | 'hooks' | 'services' | 'validation' | 'context'] = options.components
             .split(',')
-            .map((c: string) => c.trim().toLowerCase()) as ['component' | 'styles' | 'hooks' | 'types' | 'services' | 'validation' | 'context' | 'readme'];
+            .map((c: string) => c.trim().toLowerCase()) as ['component' | 'styles' | 'hooks' | 'services' | 'validation' | 'context'];
 
         const frontendConfig: FrontendGeneratorConfig = {
             app: options.app,
