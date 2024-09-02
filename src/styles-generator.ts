ce60d07af42955d771f3817ae076ecccf5bf1045
Malnati <ricardomalnati@gmail.com>
2024-09-02 13:16:02 -0300

    Refactor styles-generator.ts to remove unused imports

--- src/styles-generator.ts
+++ src/styles-generator.ts
@@ -1,7 +1,5 @@
 // src/styles-generator.ts
 
-import * as fs from 'fs';
-import * as path from 'path';
 import { IGenerator, FrontendGeneratorConfig, RequestConfig } from "./interfaces";
 import { BaseGenerator } from "./base-generator";
 
